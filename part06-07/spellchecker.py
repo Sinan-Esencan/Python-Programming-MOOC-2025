dac7bdfbb29786df4af3a16ddeeff107516fc525
Sinan-Esencan <esencansinan@gmail.com>
2025-10-06 00:22:34 +0300

    dekstoptan yukledim evet desktoptan

---
+++ part06-07/spellchecker.py
@@ -0,0 +1,38 @@
+correct_words_list = []
+with open("wordlist.txt") as read_file:
+    for line in read_file:
+        word = line.strip()
+        correct_words_list.append(word)
+# print(correct_words_list)
+
+text = input("Write text: ")
+parts = text.split(" ")
+# print(parts)
+result = ""
+for word in parts:
+    if word.lower() in correct_words_list:
+        result += word + " "
+    else:
+        result += f"*{word}* "
+print(result)
+
+
+# alt2 - mooc.fi: bence benimki dogru cunku split(' ') yerine split() kullanılmalı
+def wordlist():
+    words = []
+    with open("wordlist.txt") as file:
+        for row in file:
+            words.append(row.strip())
+    return words
+
+words = wordlist()
+sentence = input("Write text: ")
+
+for word in sentence.split(' '):
+    if word.lower() in words:
+        print(word + " ", end="")
+    else:
+        print("*" + word + "* ", end="")
+print()
+
+# write your solution here

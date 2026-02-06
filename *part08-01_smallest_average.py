825ad309e1e07d427957ae4eb4b12577bc46d1b6
Sinan Esencan <esencansinan@gmail.com>
2026-02-06 20:16:48 +0300

    Add main function to test smallest_average

--- *part08-01_smallest_average.py
+++ *part08-01_smallest_average.py
@@ -60,6 +60,12 @@ def smallest_average(person1: dict, person2: dict, person3: dict):
 
     return smallest
 
+if __name__ == "__main__":
+    person1 = {"name": "Mary", "result1": 2, "result2": 3, "result3": 3}
+    person2 = {"name": "Gary", "result1": 5, "result2": 1, "result3": 8}
+    person3 = {"name": "Larry", "result1": 3, "result2": 1, "result3": 1}
+    print(smallest_average(person1, person2, person3))
+
 
 # alt2: 3 kisi oldugunu biliyorsak ama her kisinin result sayısını bilmiyorsak:
 def average(person: dict):

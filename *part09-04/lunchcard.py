afe797b6259484acada1588b922daf5cf16a4862
Sinan Esencan <esencansinan@gmail.com>
2026-05-23 21:07:15 +0300

    Add LunchCard class with basic functionalities
    Implemented LunchCard class with methods for eating lunch, eating special, and depositing money. Added example usage of the LunchCard class.

---
+++ *part09-04/lunchcard.py
@@ -0,0 +1,57 @@
+# At Unicafe, the student cafeteria at the University of Helsinki, students can pay for their lunch with a special debit card.
+# In this exercise you will write a class called LunchCard, with the purpose of emulating the functions provided by the cafeteria's debit card.
+# instruction devamı icin: advanced course in programming > part 8 > More examples of classes > LunchCard
+
+class LunchCard:
+    def __init__(self, balance: float):
+        self.balance = balance
+    
+    def __str__(self):
+        return f"The balance is {self.balance:.1f} euros"
+    
+    def eat_special(self):
+        if self.balance - 4.6 < 0:
+            return
+        self.balance -= 4.6
+
+    def eat_lunch(self):
+        if self.balance - 2.6 < 0:
+            return
+        self.balance -= 2.6
+    
+    def deposit_money(self, deposit):
+        if deposit < 0:
+            raise ValueError("You cannot deposit an amount of money less than zero")
+        self.balance += deposit
+    
+
+# ex1,2,3
+card = LunchCard(50)
+print(card)
+
+card.eat_lunch()
+print(card)
+
+card.eat_special()
+card.eat_lunch()
+print(card)
+
+card.deposit_money(10)
+print(card)
+
+# ex4
+peters_card = LunchCard(20)
+graces_card = LunchCard(30)
+peters_card.eat_special()
+graces_card.eat_lunch()
+print("Peter:", peters_card)
+print("Grace:", graces_card)
+peters_card.deposit_money(20)
+graces_card.eat_special()
+print("Peter:", peters_card)
+print("Grace:", graces_card)
+peters_card.eat_lunch()
+peters_card.eat_lunch()
+graces_card.deposit_money(50)
+print("Peter:", peters_card)
+print("Grace:", graces_card)

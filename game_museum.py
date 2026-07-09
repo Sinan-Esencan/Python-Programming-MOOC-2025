e3af84d7a38ca2a3c6c6c5a592efc35417ee68fe
Sinan Esencan <esencansinan@gmail.com>
2026-07-09 13:25:36 +0300

    Add GameMuseum class with overridden list_games method
    Define GameMuseum class inheriting from GameWarehouse, overriding list_games to filter games before 1990.

---
+++ game_museum.py
@@ -0,0 +1,38 @@
+# The exercise template contains class definitions for a ComputerGame and a GameWarehouse. A GameWarehouse object is used to store ComputerGame objects.
+# Please familiarize yourself with these classes. Then define a new class named GameMuseum which inherits the GameWarehouse class.
+# The GameMuseum class should override the list_games() method, so that it returns a list of only those games which were made before the year 1990.
+# The new class should also have a constructor which calls the constructor from the parent class GameWarehouse. The constructor takes no arguments.
+
+# TEE RATKAISUSI TÄHÄN:
+class ComputerGame:
+    def __init__(self, name: str, publisher: str, year: int):
+        self.name = name
+        self.publisher = publisher
+        self.year = year
+
+class GameWarehouse:
+    def __init__(self):
+        self.__games = []
+
+    def add_game(self, game: ComputerGame):
+        self.__games.append(game)
+
+    def list_games(self):
+        return self.__games
+
+class GameMuseum(GameWarehouse):
+    def list_games(self):
+        games_before_1990 = []
+        for game in super().list_games():
+            if game.year < 1990:
+                games_before_1990.append(game)
+        return games_before_1990
+        
+
+if __name__ == "__main__":
+    museum = GameMuseum()
+    museum.add_game(ComputerGame("Pacman", "Namco", 1980))
+    museum.add_game(ComputerGame("GTA 2", "Rockstar", 1999))
+    museum.add_game(ComputerGame("Bubble Bobble", "Taito", 1986))
+    for game in museum.list_games():
+        print(game.name)
